0ed0b1e2c113cb283993feb5f37dff85a6d679be
jenerCruz <pamecoaxaca.telcel@gmail.com>
2026-02-13 08:05:36 -0600

    build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lowdain.app',
+  appName: 'lowdain',
+  webDir: 'dist'
+};
+
+export default config;
